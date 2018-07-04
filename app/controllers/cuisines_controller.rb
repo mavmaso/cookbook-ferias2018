dda5a8638bd97cb0e194dd1bc203a5f4739fc406
mavmaso e mufosi <mavmaso@gmail.com>
2018-07-03 22:25:45 -0300

    Cadastro cusine Incompleta

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -5,4 +5,23 @@ class CuisinesController < ApplicationController
     @cuisine = Cuisine.find(id)
   end
 
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.new(cuisine_params)
+    if @cuisine.save
+      redirect_to @cuisine
+    else
+      render :new
+    end    
+  end
+
+  private
+    
+    def cuisine_params
+      params.require(:cuisine).permit(:name)
+    end
+
 end
