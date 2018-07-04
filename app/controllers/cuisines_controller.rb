8ac44e6cce9f707545ef9bd68a435df6bc825d9e
mavmaso e mufosi <mavmaso@gmail.com>
2018-07-03 21:45:39 -0300

    visitante filtra por cozinha

---
+++ app/controllers/cuisines_controller.rb
@@ -0,0 +1,8 @@
+class CuisinesController < ApplicationController
+
+  def show
+    id=params[:id]
+    @cuisine = Cuisine.find(id)
+  end
+
+end
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,3 @@
 class Cuisine < ApplicationRecord
-  has_many :recipe
+  has_many :recipes
 end
